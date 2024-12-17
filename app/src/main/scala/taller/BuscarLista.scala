f7828c582d59ec67c486da4dede1c90bf80296bd
Sebastian Ceron Orozco <117455780+Sebastian-Ceron-Orozco@users.noreply.github.com>
2024-12-16 23:38:57 -0500

    funciones nuevas
    subo las funciones de riego optimo, fincapar y movilidadpar

--- app/src/main/scala/taller/BuscarLista.scala
+++ app/src/main/scala/taller/BuscarLista.scala
@@ -1,4 +1,6 @@
 package taller
+import scala.util.Random
+import scala.collection.parallel.CollectionConverters._
 
 class BuscarLista() {
   // el tiempo de riego y la prioridad del tablon
@@ -25,4 +27,26 @@ class BuscarLista() {
     case x :: xs => if (x == elemento) true else buscarElemento(xs, elemento)
    }
   }
+
+  def ProgramacionRiegoOptimo(f: Finca, d: Distancia): (ProgRiego, Int) = {
+    // Dada una finca devuelve la programación
+    // de riego óptima
+    val programaciones = generarProgramacionesRiego(f)
+    val costos = programaciones.map(pi =>
+      (pi, costoRiegoFinca(f, pi) + costoMovilidad(f, pi, d))
+    )
+    costos.minBy(_._2)
+  }
+
+  def costoRiegoFincaPar(f: Finca, pi: ProgRiego): Int = {
+    // Devuelve el costo total de regar una finca f dada una
+    // programación de riego pi, calculando en paralelo
+    (0 until f.length).par.map(i => costoRiegoTablon(i, f, pi)).sum
+  }
+
+  def costoMovilidadPar(f: Finca, pi: ProgRiego, d: Distancia): Int = {
+    // Calcula el costo de movilidad de manera paralela
+    (0 until pi.length - 1).par.map(j => d(pi(j))(pi(j + 1))).sum
+  }
+
 }
