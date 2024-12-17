372d125fa55d3949a80daf47dc75f355ede506da
osorioceba <72362552+osorioceba@users.noreply.github.com>
2024-12-16 23:19:30 -0500

    v1

--- app/src/main/scala/taller/BuscarLista.scala
+++ app/src/main/scala/taller/BuscarLista.scala
@@ -1,6 +1,24 @@
 package taller
 
 class BuscarLista() {
+  // el tiempo de riego y la prioridad del tablon
+  type Tablon = (Int, Int, Int)
+
+  // Una finca es un vector de tablones
+  type Finca = Vector[Tablon]
+
+  // La distancia entre dos tablones se representa por una matriz
+  type Distancia = Vector[Vector[Int]]
+
+  // Una programación de riego es un vector que asocia
+  // cada tablon i con su turno de riego (0 es el primer turno,
+  // n-1 es el último turno)
+  type ProgRiego = Vector[Int]
+
+  // El tiempo de inicio de riego es un vector que asocia
+  // cada tablon i con el momento del tiempo en que se riega
+  type TiempoInicioRiego = Vector[Int]
+  
   def buscarElemento(lista: List[Int], elemento: Int): Boolean = {
   lista match {
     case Nil => false
