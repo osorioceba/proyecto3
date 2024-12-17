7e7b58a408514c8431328ce6aebe9cd772096674
osorioceba <72362552+osorioceba@users.noreply.github.com>
2024-12-16 23:31:11 -0500

    v2

--- app/src/main/scala/taller/BuscarLista.scala
+++ app/src/main/scala/taller/BuscarLista.scala
@@ -1,4 +1,5 @@
 package taller
+import scala.util.Random
 
 class BuscarLista() {
   // el tiempo de riego y la prioridad del tablon
@@ -18,11 +19,38 @@ class BuscarLista() {
   // El tiempo de inicio de riego es un vector que asocia
   // cada tablon i con el momento del tiempo en que se riega
   type TiempoInicioRiego = Vector[Int]
-  
+
   def buscarElemento(lista: List[Int], elemento: Int): Boolean = {
   lista match {
     case Nil => false
     case x :: xs => if (x == elemento) true else buscarElemento(xs, elemento)
    }
+  }
+// Si t : TiempoInicioRiego y t.length == n, t(i) es la hora a
+  // la que inicia a regarse el tablon i
+  val random = new Random()
+
+  def fincaAlazar(long: Int): Finca = {
+    // Crea una finca de long tablones,
+    // con valores aleatorios entre 1 y long * 2 para el tiempo
+    // de supervivencia, entre 1 y long para el tiempo
+    // de regado y entre 1 y 4 para la prioridad
+    val v = Vector.fill(long)(
+      (random.nextInt(long * 2) + 1, 
+      random.nextInt(long) + 1, 
+      random.nextInt(4) + 1)
+    )
+    v
+  }
+def distanciaAlazar(long: Int): Distancia = {
+    // Crea una matriz de distancias para una finca
+    // de long tablones, con valores aleatorios entre
+    // 1 y long * 3
+    val v = Vector.fill(long, long)(random.nextInt(long * 3) + 1)
+    Vector.tabulate(long, long)((i, j) =>
+      if (i < j) v(i)(j)
+      else if (i == j) 0
+      else v(j)(i)
+    )
   }
 }
